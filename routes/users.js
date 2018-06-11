5b8e65b12ffaa1c81ed26a2d9a00ec7f6e93fb1c
harleyjj <harleyyesm@gmail.com>
2018-06-11 12:52:44 -0700

    updated POST /users endpoint to use .hashPassword

--- routes/users.js
+++ routes/users.js
@@ -26,13 +26,21 @@ router.post('/users', (req, res, next) => {
   }
 
 
-  User.create(newUser)
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
     .then(result => {
-      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+      return res.status(201).location(`/api/users/${result.id}`).json(result);
     })
     .catch(err => {
       if (err.code === 11000) {
-        err = new Error('Username already exists');
+        err = new Error('The username already exists');
         err.status = 400;
       }
       next(err);
